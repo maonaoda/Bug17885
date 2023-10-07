9bab03f0465f039d860ab4431a4c92ffc814ec06
wangyk <wangyk@liandisy.com.cn>
2023-10-07 17:36:28 +0900

    add

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1
+{
+    public class TestModle : ObservableObject
+    {
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        private bool showResultCollectionView;
+
+        public bool ShowResultCollectionView
+        {
+            get => showResultCollectionView;
+            set => SetProperty(ref showResultCollectionView, value);
+        }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+
+        public ObservableCollection<String> Users { get; set; }
+
+        public TestModle User { get; set; }
+
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Users = new ObservableCollection<string>();
+            Users.Add("AAA");
+            Users.Add("BBB");
+            Users.Add("CCC");
+
+            User = new TestModle();
+            User.Name = "AAA";
+
+            this.BindingContext = this;
+        }
+
+        private void CounterBtn_Clicked(object sender, EventArgs e)
+        {
+            User.ShowResultCollectionView = !User.ShowResultCollectionView;
+        }
+    }
+}
